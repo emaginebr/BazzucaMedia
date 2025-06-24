de52be3e5ad216b9d6695545d59f8ecba8abc003
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2025-06-24 20:35:17 -0300

    change name to Bazzuca Media

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker stop bazzuca-api1
 docker rm bazzuca-api1
-cd ./Backend/BazzucaMedia/BazzucaMedia.API
-docker build -t bazzuca-api -f Dockerfile .
+cd ./Backend/BazzucaMedia
+docker build -t bazzuca-api -f BazzucaMedia.API/Dockerfile .
 docker run --name bazzuca-api1 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORTS=443 --network docker-network bazzuca-api &
 docker ps
