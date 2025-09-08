af763befaf5178ff957672f20d728da0367c5499
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2025-09-08 09:15:41 -0300

    update nauth

--- update-nauth.sh
+++ update-nauth.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd ../NAuth/Frontend/nauth-core
-#npm install
-#npm run build
+cd ../NAuth/Backend/NAuth
+pwd
+dotnet build -c Release NAuth.sln
+cd ./NAuth.Client/bin/Release/net8.0
+pwd
+cp NAuth.Client.dll ../../../../../../../BazzucaMedia/Backend/BazzucaMedia/Lib
+cp NAuth.DTO.dll ../../../../../../../BazzucaMedia/Backend/BazzucaMedia/Lib
+cd ../../../../../../../NAuth/Frontend/nauth-core
+pwd
+npm install
+npm run build
 cp -Rf ./dist/* ../../../BazzucaMedia/Frontend/bazzuca-app/src/nauth-core
